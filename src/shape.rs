32710f828327596664c01a96203ceeea7ec770cd
CalebCouch <calebblackcat@gmail.com>
2025-03-13 12:26:28 -0400

    test

--- src/shape.rs
+++ src/shape.rs
@@ -42,16 +42,23 @@ impl ShapeAtlas {
 pub struct Parametric(u32, u32);
 
 impl Parametric {
-    pub fn build(self, mut iter: impl FnMut(f64, f64, f64)) {
+    pub fn build(self, mut iter: impl FnMut(f64, f64, f64, Box<dyn Fn(f64, f64) -> (u32, u32)>)) {
         let hsize = (self.0 as f64/2.0, self.1 as f64/2.0);
         let steps = self.0*self.1;
         let increment = std::f64::consts::PI/(steps as f64 / 2.0);
 
         let a = hsize.0-0.5;
         let b = hsize.1-0.5;
+
+        let to_coord = move |x: f64, y: f64| -> (u32, u32) {
+            let x = ((a)+x).round();
+            let y = ((b)+y).round();
+            ((x as u32).min(self.0-1), (y as u32).min(self.1-1))
+        };
+
         for i in 0..steps {
             let t = -std::f64::consts::PI + (increment*i as f64);
-            iter(a, b, t)
+            iter(a, b, t, Box::new(to_coord))
         }
     }
 }
@@ -63,62 +70,114 @@ pub struct Ellipse {
     pub size: (u32, u32),
 }
 
+use std::time::Instant;
+
 impl Shape for Ellipse {
     fn build(self) -> Image {
-        let size = (self.size.0, self.size.1);
+        let time = Instant::now();
+        println!("margin: {}", time.elapsed().as_millis());
+        let alpha = 4;
+        let size = (self.size.0*alpha, self.size.1*alpha);
+        let stroke = self.stroke.min(self.size.0/2)*alpha;
 
         let mut image = image_crate::DynamicImage::new(size.0, size.1, image_crate::ColorType::Rgba8);
-        for x in 0..size.0 {
-            for y in 0..size.1 {
-                image.put_pixel(x, y, image_crate::Rgba([200, 200, 0, 255]));
+        let pixel = image_crate::Rgba([self.color.0, self.color.1, self.color.2, self.color.3]);
+        let non_pixel = image_crate::Rgba([0, 0, 0, 0]);
+
+        println!("start: {}", time.elapsed().as_millis());
+      //for x in 0..size.0 {
+      //    for y in 0..size.1 {
+      //        image.put_pixel(x, y, non_pixel);
+      //    }
+      //}
+      //println!("filled: {}", time.elapsed().as_millis());
+
+        for x in 0..stroke {
+            for y in 0..stroke {
+                image.put_pixel(x, y, pixel);
+                image.put_pixel(x, size.1-y-1, pixel);
+                image.put_pixel(size.0-x-1, size.1-y-1, pixel);
+                image.put_pixel(size.0-x-1, y, pixel);
             }
         }
+        println!("corners: {}", time.elapsed().as_millis());
 
-        let pixel = image_crate::Rgba([self.color.0, self.color.1, self.color.2, self.color.3]);
-        let k = (self.stroke as f64)*-4.0;
+        if stroke > 0 {
+            let k = -(stroke as f64);
+            Parametric(size.0, size.1).build(
+                |a: f64, b: f64, t: f64, to_coord: Box<dyn Fn(f64, f64) -> (u32, u32)>| {
+                let x = (a + (b*k / (((a*a)*(t.sin()*t.sin()))+((b*b)*(t.cos()*t.cos()))).sqrt())) * t.cos();
+                let y = (b + (a*k / (((a*a)*(t.sin()*t.sin()))+((b*b)*(t.cos()*t.cos()))).sqrt())) * t.sin();
 
-        Parametric(size.0, size.1).build(|a: f64, b: f64, t: f64| {
-            let x = (a) * t.cos();
-            let y = (b) * t.sin();
-
-            let x = ((a)+x).round();
-            let y = ((b)+y).round();
-
-            let x = (x as u32).min(size.0-1);
-            let y = (y as u32).min(size.1-1);
-
-            if (x as f64) < a {
-                for i in x..(a.ceil() as u32) {
-                    image.put_pixel(i, y, pixel);
+                let (x, y) = to_coord(x, y);
+                if (x as f64) < a {
+                    for i in 0..x+1 {
+                        image.put_pixel(i, y, pixel);
+                    }
+                } else {
+                    for i in x..1+(a+a) as u32 {
+                        image.put_pixel(i, y, pixel);
+                    }
                 }
-            } else {
-                for i in (a.ceil() as u32)..x+1 {
-                    image.put_pixel(i, y, pixel);
+
+                if (y as f64) < b {
+                    for i in 0..y+1 {
+                        image.put_pixel(x, i, pixel);
+                    }
+                } else {
+                    for i in y..1+(b+b) as u32 {
+                        image.put_pixel(x, i, pixel);
+                    }
                 }
-            }
-        });
+            });
+            println!("stroked: {}", time.elapsed().as_millis());
 
-          //let x = (a + (b*k / (((a*a)*(t.sin()*t.sin()))+((b*b)*(t.cos()*t.cos()))).sqrt())) * t.cos();
-          //let y = (b + (a*k / (((a*a)*(t.sin()*t.sin()))+((b*b)*(t.cos()*t.cos()))).sqrt())) * t.sin();
+            Parametric(size.0, size.1).build(
+                |a: f64, b: f64, t: f64, to_coord: Box<dyn Fn(f64, f64) -> (u32, u32)>| {
+                let x = (a) * t.cos();
+                let y = (b) * t.sin();
 
-          //let x = (0.5+a+x).round();
-          //let y = (0.5+b+y).round();
+                let (x, y) = to_coord(x, y);
 
-          //let x = (x as u32).min(size.0-1);
-          //let y = (y as u32).min(size.1-1);
+                if (x as f64) < a {
+                    for i in 0..x+1 {
+                        image.put_pixel(i, y, non_pixel);
+                    }
+                } else {
+                    for i in x..1+(a+a) as u32 {
+                        image.put_pixel(i, y, non_pixel);
+                    }
+                }
+            });
+            println!("ellipse: {}", time.elapsed().as_millis());
+        } else {
+            Parametric(size.0, size.1).build(
+                |a: f64, b: f64, t: f64, to_coord: Box<dyn Fn(f64, f64) -> (u32, u32)>| {
+                let x = (a) * t.cos();
+                let y = (b) * t.sin();
 
-          //image.put_pixel(
-          //    x, y,
-          //    pixel
-          //);
+                let (x, y) = to_coord(x, y);
+
+                if (x as f64) < a {
+                    for i in x..(a.ceil() as u32) {
+                        image.put_pixel(i, y, pixel);
+                    }
+                } else {
+                    for i in (a.ceil() as u32)..x as u32 {
+                        image.put_pixel(i, y, pixel);
+                    }
+                }
+            });
+        }
 
 
-      //let mut dst_image = image_crate::DynamicImage::new(
-      //    self.size.0, self.size.1, image_crate::ColorType::Rgba8
-      //);
-      //let mut resizer = Resizer::new();
-      //resizer.resize(&image, &mut dst_image, &None).unwrap();
+        let mut dst_image = image_crate::DynamicImage::new(
+            self.size.0, self.size.1, image_crate::ColorType::Rgba8
+        );
+        let mut resizer = Resizer::new();
+        resizer.resize(&image, &mut dst_image, &None).unwrap();
+        println!("resized: {}", time.elapsed().as_millis());
 
-        image.into()
+        dst_image.into()
     }
 }
