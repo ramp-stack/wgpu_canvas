0c5767440b7c1fdfe46d867d4df037b481ad556f
CalebCouch <calebblackcat@gmail.com>
2025-04-22 16:27:28 -0400

    test

--- src/text.rs
+++ src/text.rs
@@ -4,14 +4,22 @@ use glyphon::fontdb::{Database, Source, ID};
 use glyphon::cosmic_text::Align;
 use glyphon::cosmic_text::LineEnding;
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
 
 use super::{Area, Color};
 
-#[derive(Clone, Debug)]
+//  #[derive(Debug)]
+//  pub struct CloneMutex<T>(Mutex<T>);
+//  impl<T: Clone> Clone for CloneMutex<T> {
+//      fn clone(&self) -> Self {
+//          CloneMutex(Mutex::new(self.0.lock().unwrap().clone()))
+//      }
+//  }
+
+#[derive(Debug)]
 pub struct Text{
-    buffer: Buffer,
+    buffer: Mutex<Buffer>,
     pub text: String,
     pub color: Color,
     pub font: Font,
@@ -19,6 +27,17 @@ pub struct Text{
     pub line_height: f32,
     pub width: Option<f32>,
     pub align: Align,
+}
+
+impl Clone for Text {
+    fn clone(&self) -> Self {
+        Text{buffer: Mutex::new(self.buffer.lock().unwrap().clone()),
+            text: self.text.clone(), color: self.color.clone(),
+            font: self.font.clone(), size: self.size.clone(),
+            line_height: self.line_height.clone(), width: self.width.clone(),
+            align: self.align.clone()
+        }
+    }
 }
 
 impl Text {
@@ -33,28 +52,40 @@ impl Text {
         buffer.set_size(font_system.as_mut(), width.map(|w| 1.0+w), None);
         buffer.set_text(font_system.as_mut(), text, font.1, Shaping::Basic);
         buffer.lines.iter_mut().for_each(|l| {l.set_align(Some(align));});
-        Text{buffer, text: text.to_string(), color, font, align, size, line_height, width}
+        Text{
+            buffer: Mutex::new(buffer),
+            text: text.to_string(), color, font, align,
+            size, line_height, width
+        }
     }
 
-    pub fn size(&self) -> (f32, f32) {
-        let newline = self.buffer.lines.last().and_then(|line| (!self.text.is_empty() && line.ending() != LineEnding::None).then_some(self.line_height)).unwrap_or_default();
-        let (w, h) = self.buffer.lines.iter().fold((0.0f32, 0.0f32), |(mw, mh), line| {
-            let (w, h) = line.layout_opt().as_ref().unwrap().iter().fold((0.0f32, 0.0f32), |(w, h), span|
-                (w.max(span.w), h+span.line_height_opt.unwrap_or(span.max_ascent+span.max_descent))
+    pub fn size(&self, font_system: &mut impl AsMut<FontSystem>) -> (f32, f32) {
+        self.update_buffer(font_system, 0);
+        let mut buffer = self.buffer.lock().unwrap();
+        let (mut w, mut h, mut i) = (0.0f32, 0.0, 0);
+        while let Some(line) = buffer.line_layout(font_system.as_mut(), i) {
+            i += 1;
+            let (lw, lh) = line.iter().fold((0.0f32, 0.0f32), |(w, h), span|
+                (w.max(span.w), h+span.line_height_opt.unwrap_or(
+                    span.max_ascent+span.max_descent
+                ))
             );
-            (mw.max(w), mh+h.max(self.line_height))
-        });
+            w = w.max(lw);
+            h+=lh.max(self.line_height);
+        }
+        let newline = buffer.lines.last().and_then(|line| (!self.text.is_empty() && line.ending() != LineEnding::None).then_some(self.line_height)).unwrap_or_default();
         (w, h+newline)
     }
 
-    fn update_buffer(&mut self, font_system: &mut impl AsMut<FontSystem>, z: usize) {
+    fn update_buffer(&self, font_system: &mut impl AsMut<FontSystem>, z: usize) {
+        let mut buffer = self.buffer.lock().unwrap();
         //TODO: check if text changed first
         // pub fn get_text(&self) -> String {self.0.lines.iter().fold(String::new(), |a, l| a+"\n"+l.text())}
-        self.buffer.set_metrics(font_system.as_mut(), Metrics::new(self.size, self.line_height));
-        self.buffer.set_wrap(font_system.as_mut(), Wrap::WordOrGlyph);
-        self.buffer.set_size(font_system.as_mut(), self.width.map(|w| 1.0+w), None);
-        self.buffer.set_text(font_system.as_mut(), &self.text, self.font.1.metadata(z), Shaping::Basic);
-        self.buffer.lines.iter_mut().for_each(|l| {l.set_align(Some(self.align));});
+        buffer.set_metrics(font_system.as_mut(), Metrics::new(self.size, self.line_height));
+        buffer.set_wrap(font_system.as_mut(), Wrap::WordOrGlyph);
+        buffer.set_size(font_system.as_mut(), self.width.map(|w| 1.0+w), None);
+        buffer.set_text(font_system.as_mut(), &self.text, self.font.1.metadata(z), Shaping::Basic);
+        buffer.lines.iter_mut().for_each(|l| {l.set_align(Some(self.align));});
     }
 }
 
@@ -138,20 +169,21 @@ impl TextRenderer {
         width: f32,
         height: f32,
         font_atlas: &mut FontAtlas,
-        mut text_areas: Vec<(u16, Area, Text)>
+        text_areas: Vec<(u16, Area, Text)>
     ) {
         font_atlas.trim();
         self.text_atlas.trim();
         self.viewport.update(queue, Resolution{width: width as u32, height: height as u32});
-
-        text_areas.iter_mut().for_each(|(z, _, t)| {
-            t.update_buffer(font_atlas, *z as usize);
-        });
-        let text_areas = text_areas.iter_mut().map(|(_, a, t)| {
-            let default_color = glyphon::Color::rgba(t.color.0, t.color.1, t.color.2, t.color.3);
+        let text_areas = text_areas.into_iter().map(|(z, a, t)| {
+            t.update_buffer(font_atlas, z as usize);
+            let c = glyphon::Color::rgba(t.color.0, t.color.1, t.color.2, t.color.3);
+            let b = t.buffer.into_inner().unwrap();
+            (b, a, c)
+        }).collect::<Vec<_>>();
+        let text_areas = text_areas.iter().map(|(b, a, c)| {
             let bounds = a.bounds(width, height);
             glyphon::TextArea{
-                buffer: &t.buffer,
+                buffer: b,
                 left: a.0.0,
                 top: a.0.1,
                 scale: 1.0,
@@ -161,7 +193,7 @@ impl TextRenderer {
                     right: (bounds.0 + bounds.2).ceil() as i32,
                     bottom: (bounds.1 + bounds.3).ceil() as i32+2,//TODO: Find out why this is +2
                 },
-                default_color,
+                default_color: *c,
                 custom_glyphs: &[]
             }
         });
